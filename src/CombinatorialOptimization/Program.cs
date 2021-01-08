c73d92dd565d27873505a458c1406733857e0d5a
Valentin <fritz.valentin@live.fr>
2021-01-08 16:50:02 +0100

    Small fix

--- src/CombinatorialOptimization/Program.cs
+++ src/CombinatorialOptimization/Program.cs
@@ -20,13 +20,19 @@ namespace CombinatorialOptimization
         private static int FitnessCalculation(Individual individual)
         {
             var sum = 0;
-            var prod = 1;
+            var prod = 0;
             for (var i = 0; i < individual.Chromosome.Length; i++)
             {
                 if (individual.Chromosome[i] == 0)
+                {
                     sum += i + 1;
+                }
                 else
+                {
+                    if (prod == 0)
+                        prod = 1;
                     prod *= i + 1;
+                }
             }
 
             return Math.Abs(sum - 36) + Math.Abs(prod - 360);
